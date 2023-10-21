91df7ec5edd0c4ce6f3056f0759f991ab980cd84
Ruchi Reddy <ruchireddy@Ruchis-MacBook-Pro.local>
2023-10-21 12:10:12 -0400

    recording on webpage

---
+++ node_modules/ts-ebml/lib/cli.js
@@ -0,0 +1,105 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _1 = require("./");
+var com = require("commander");
+var fs = require("fs");
+var version = require("../package.json").version;
+com
+    .version(version)
+    .usage("[options] <*.webm>")
+    .option('-s, --seekable', 'try convert MediaRecorder WebM to seekable WebM and write buffer stdout, like `ts-ebml -s not_seekable.webm | cat > seekable.webm`')
+    .option('-k, --keyframe', 'TimecodeScale & Timecode & SimpleBlock(VideoTrack && keyframe) ebml elements pass filter for thumbnails(Random Access Points)')
+    .arguments('<*.webm>')
+    .parse(process.argv);
+var args = com.args;
+if (args.length < 1) {
+    process.exit();
+}
+if (com.seekable) {
+    var decoder = new _1.Decoder();
+    var reader_1 = new _1.Reader();
+    reader_1.logging = false;
+    reader_1.drop_default_duration = false;
+    var buf = fs.readFileSync(args[0]);
+    var elms = decoder.decode(buf);
+    elms.forEach(function (elm) { reader_1.read(elm); });
+    reader_1.stop();
+    var refinedMetadataBuf = _1.tools.makeMetadataSeekable(reader_1.metadatas, reader_1.duration, reader_1.cues);
+    var body = buf.slice(reader_1.metadataSize);
+    var refined = new Buffer(_1.tools.concat([new Buffer(refinedMetadataBuf), body]).buffer);
+    process.stdout.write(refined);
+}
+else if (com.keyframe) {
+    var decoder_1 = new _1.Decoder();
+    var TrackType_1 = -1;
+    var TrackNumber_1 = -1;
+    var CodecID_1 = "";
+    var trackTypes_1 = {};
+    fs.createReadStream(args[0]).on('data', function (buf) {
+        var ebmlElms = decoder_1.decode(buf);
+        ebmlElms.forEach(function (elm) {
+            if (elm.type === "m" && elm.name === "TrackEntry" && elm.isEnd) {
+                trackTypes_1[TrackNumber_1] = { TrackType: TrackType_1, CodecID: CodecID_1 };
+                TrackType_1 = -1;
+                TrackNumber_1 = -1;
+                CodecID_1 = "";
+            }
+            else if (elm.type === "u" && elm.name === "TrackType") {
+                TrackType_1 = elm.value;
+            }
+            else if (elm.type === "u" && elm.name === "TrackNumber") {
+                TrackNumber_1 = elm.value;
+            }
+            else if (elm.type === "s" && elm.name === "CodecID") {
+                CodecID_1 = elm.value;
+            }
+            else if (elm.type === "u" && elm.name === "TimecodeScale") {
+                process.stdout.write(new Buffer(new _1.Encoder().encode([elm])));
+            }
+            else if (elm.type === "u" && elm.name === "Timecode") {
+                process.stdout.write(new Buffer(new _1.Encoder().encode([elm])));
+            }
+            else if (elm.type === "b" && elm.name === "SimpleBlock") {
+                var o = _1.tools.ebmlBlock(elm.data);
+                var _a = trackTypes_1[o.trackNumber], type = _a.TrackType, id = _a.CodecID;
+                // 1 means video
+                if (type === 1 && o.keyframe && (id === "V_VP9" || id === "V_VP8")) {
+                    process.stdout.write(new Buffer(new _1.Encoder().encode([elm])));
+                }
+            }
+        });
+    });
+}
+else {
+    var decoder_2 = new _1.Decoder();
+    fs.createReadStream(args[0]).on('data', function (buf) {
+        // put ebml info
+        var ebmlElms = decoder_2.decode(buf);
+        ebmlElms.forEach(function (elm) {
+            var name = elm.name, type = elm.type, tagStart = elm.tagStart, level = elm.level;
+            if (elm.type === "m") {
+                if (!elm.isEnd) {
+                    console.log(tagStart + "\t" + type + "\t" + level + "\t" + name);
+                }
+            }
+            else {
+                if (elm.type === "b") {
+                    if (elm.name === "SimpleBlock") {
+                        var _a = _1.tools.ebmlBlock(elm.value), discardable = _a.discardable, frames_1 = _a.frames, invisible = _a.invisible, keyframe = _a.keyframe, timecode = _a.timecode, trackNumber = _a.trackNumber;
+                        console.log(tagStart + "\t" + type + "\t" + level + "\t" + name, "track:" + trackNumber + " timecode:" + timecode + "\tkeyframe:" + keyframe + "\tinvisible:" + invisible + "\tdiscardable:" + discardable + "\tlacying:" + frames_1.length);
+                    }
+                    else {
+                        console.log(tagStart + "\t" + type + "\t" + level + "\t" + name, "<Buffer " + elm.value.byteLength + ">");
+                    }
+                }
+                else if (elm.type === "d") {
+                    console.log(tagStart + "\t" + type + "\t" + level + "\t" + name, _1.tools.convertEBMLDateToJSDate(elm.value));
+                }
+                else {
+                    console.log(tagStart + "\t" + type + "\t" + level + "\t" + name, elm.value);
+                }
+            }
+        });
+    });
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,130 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import "./styles.css";
+import { useReactMediaRecorder } from "react-media-recorder";
+import VideoRecorder from "react-video-recorder";
+import {
+  RecordWebcam,
+  useRecordWebcam,
+  CAMERA_STATUS
+} from "react-record-webcam";
+const OPTIONS = {
+  filename: "test-filename",
+  fileType: "mp4",
+  width: 1920,
+  height: 1080
+};
+const RecordView = () => {
+  const {
+    status,
+    startRecording,
+    stopRecording,
+    mediaBlobUrl
+  } = useReactMediaRecorder({
+    video: true,
+    facingMode: { exact: "environment" }
+  });
 
-function App() {
+  return (
+    <div>
+      <p>{status}</p>
+      <button onClick={startRecording}>Start Recording</button>
+      <button onClick={stopRecording}>Stop Recording</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+    </div>
+  );
+};
 
-    return (<div className="container">
-        <BrowserRouter>
-            <div className="row mt-2">
-                <div className="col-2">
-                    <h1>Test shit</h1>
-                </div>
-                <div className="col-10">
-                    {/* <Routes>
-                        <Route path="/"
-                               element={<HomeComponent/>}/>
-                    </Routes> */}
-                    <h1>Hello World</h1>
-                </div>
-            </div>
-        </BrowserRouter>
-    </div>);
+export default function App() {
+  const recordWebcam = useRecordWebcam(OPTIONS);
+  const getRecordingFileHooks = async () => {
+    const blob = await recordWebcam.getRecording();
+    console.log({ blob });
+  };
+
+  const getRecordingFileRenderProp = async (blob) => {
+    console.log({ blob });
+  };
+  return (
+    <div>
+      <h1>3.react-record-webcam</h1>
+      <p>Camera status: {recordWebcam.status}</p>
+      <div>
+        <button
+          disabled={
+            recordWebcam.status === CAMERA_STATUS.OPEN ||
+            recordWebcam.status === CAMERA_STATUS.RECORDING ||
+            recordWebcam.status === CAMERA_STATUS.PREVIEW
+          }
+          onClick={recordWebcam.open}
+        >
+          Open camera
+        </button>
+        <button
+          disabled={
+            recordWebcam.status === CAMERA_STATUS.CLOSED ||
+            recordWebcam.status === CAMERA_STATUS.PREVIEW
+          }
+          onClick={recordWebcam.close}
+        >
+          Close camera
+        </button>
+        <button
+          disabled={
+            recordWebcam.status === CAMERA_STATUS.CLOSED ||
+            recordWebcam.status === CAMERA_STATUS.RECORDING ||
+            recordWebcam.status === CAMERA_STATUS.PREVIEW
+          }
+          onClick={recordWebcam.start}
+        >
+          Start recording
+        </button>
+        <button
+          disabled={recordWebcam.status !== CAMERA_STATUS.RECORDING}
+          onClick={recordWebcam.stop}
+        >
+          Stop recording
+        </button>
+        <button
+          disabled={recordWebcam.status !== CAMERA_STATUS.PREVIEW}
+          onClick={recordWebcam.retake}
+        >
+          Retake
+        </button>
+        <button
+          disabled={recordWebcam.status !== CAMERA_STATUS.PREVIEW}
+          onClick={recordWebcam.download}
+        >
+          Download
+        </button>
+        <button
+          disabled={recordWebcam.status !== CAMERA_STATUS.PREVIEW}
+          onClick={getRecordingFileHooks}
+        >
+          Get recording
+        </button>
+      </div>
+
+      <video
+        ref={recordWebcam.webcamRef}
+        style={{
+          display: `${
+            recordWebcam.status === CAMERA_STATUS.OPEN ||
+            recordWebcam.status === CAMERA_STATUS.RECORDING
+              ? "block"
+              : "none"
+          }`
+        }}
+        autoPlay
+        muted
+      />
+      <video
+        ref={recordWebcam.previewRef}
+        style={{
+          display: `${
+            recordWebcam.status === CAMERA_STATUS.PREVIEW ? "block" : "none"
+          }`
+        }}
+        controls
+      />
+    </div>
+  );
 }
-
-export default App;
